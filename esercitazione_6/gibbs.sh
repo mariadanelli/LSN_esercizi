bf18fee66a4e52f7a002354bfcbedcd03aa27fff
mariadanelli <126794183+mariadanelli@users.noreply.github.com>
2023-11-02 17:41:15 +0100

    Delete esercitazione_6 directory

--- esercitazione_6/gibbs.sh
+++
@@ -1,5 +0,0 @@
-./Monte_Carlo_ISING_1D.exe
-mv EnediT.dat gibbs/EnediT.dat
-mv ChidiT.dat gibbs/ChidiT.dat
-mv MagdiT.dat gibbs/MagdiT.dat
-mv HeatdiT.dat gibbs/HeatdiT.dat
--- esercitazione_6/metropolis.sh
+++
@@ -1,5 +0,0 @@
-./Monte_Carlo_ISING_1D.exe
-mv EnediT.dat metropolis/EnediT.dat
-mv ChidiT.dat metropolis/ChidiT.dat
-mv MagdiT.dat metropolis/MagdiT.dat
-mv HeatdiT.dat metropolis/HeatdiT.dat
