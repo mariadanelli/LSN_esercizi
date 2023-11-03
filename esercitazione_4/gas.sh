ee1247266304128916cda24468cf3c7bb1c92af1
mariadanelli <126794183+mariadanelli@users.noreply.github.com>
2023-11-03 12:02:48 +0100

    Delete esercitazione_4 directory

--- esercitazione_4/gas.sh
+++
@@ -1,9 +0,0 @@
-rm *.dat
-cp input.gas input.in
-./NVE_NVT.exe
-mv output_tempeq.dat 'Equilibrio temperatura'/gas/output_tempeq.dat
-mv output_temp.dat 'Equilibrio temperatura'/gas/output_temp.dat
-mv output_press.dat 'Equilibrio temperatura'/gas/output_press.dat
-mv output_ekin.dat 'Equilibrio temperatura'/gas/output_ekin.dat
-mv output_epot.dat 'Equilibrio temperatura'/gas/output_epot.dat
-mv output_etot.dat 'Equilibrio temperatura'/gas/output_etot.dat
--- esercitazione_4/solid.sh
+++
@@ -1,9 +0,0 @@
-rm *.dat
-cp input.solid input.in
-./NVE_NVT.exe
-mv output_tempeq.dat 'Equilibrio temperatura'/solido/output_tempeq.dat
-mv output_temp.dat 'Equilibrio temperatura'/solido/output_temp.dat
-mv output_press.dat 'Equilibrio temperatura'/solido/output_press.dat
-mv output_ekin.dat 'Equilibrio temperatura'/solido/output_ekin.dat
-mv output_epot.dat 'Equilibrio temperatura'/solido/output_epot.dat
-mv output_etot.dat 'Equilibrio temperatura'/solido/output_etot.dat
