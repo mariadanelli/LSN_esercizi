1e62bee35277fa93d4020a077843a5e53bfa2766
mariadanelli <126794183+mariadanelli@users.noreply.github.com>
2023-11-03 14:34:21 +0100

    Add files via upload

---
+++ esercitazione_3/funzioni.h
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double error(double, double, int);
+double max (double, double);
+vector <double> MediaBlocchi (double, double, vector <double>);
