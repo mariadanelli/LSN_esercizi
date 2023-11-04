2cf0971b54587d5f0e01c81a73cc78ed89c0f26f
mariadanelli <126794183+mariadanelli@users.noreply.github.com>
2023-11-04 16:46:26 +0100

    Add files via upload

---
+++ esercitazione_2/2.1/Integral.h.cpp
@@ -0,0 +1,22 @@
+#ifndef __Integral__
+#define __Integral__
+#include <cmath>
+#include "FunzioneBase.h"
+#include "random.h"
+
+class Integral {
+public:Integral () {m_R= new Random();};
+	~Integral(){;};
+
+
+double IntegraleMedia(double x, int passi, FunzioneBase &f) {
+
+		double somma=0;
+	for (int i=0; i<passi; i++) {
+		somma= somma+f.Eval(x);
+	}
+return somma/double(passi);}
+private: Random *m_R;
+};
+
+#endif 
